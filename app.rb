e20624df79c276c0c4eee358ac040951bb0e7653
Daniel <demartinezgatell@gmail.com>
2019-11-26 16:50:20 +0000

    using forms

--- app.rb
+++ app.rb
@@ -10,7 +10,12 @@ get '/secret' do
   'Goodbye world!'
 end
 
-get '/cat' do 
+get '/cat' do
   @rand_name = ["daniel", "big tuna", "danny"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @rand_name = params[:name]
   erb(:index)
 end

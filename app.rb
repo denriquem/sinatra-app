9718e7d52750992dc7ef18bc323cba5f02974fbf
Daniel <demartinezgatell@gmail.com>
2019-11-27 15:08:47 +0000

    post request

--- app.rb
+++ app.rb
@@ -19,3 +19,13 @@ get '/named-cat' do
   @rand_name = params[:name]
   erb(:index)
 end
+
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
+  p params
+  @rand_name = params[:name]
+  erb(:index)
+end
